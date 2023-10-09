9d43d087d5bc7c3e54a89b1a12958f322518b460
Tossapom <chtdhkvjfsko17@gmail.com>
2023-10-09 16:06:48 +0700

    state

---
+++ src/StartState.java
@@ -0,0 +1,9 @@
+
+public class StartState implements State {
+    public void doAction() {
+        System.out.println("Start State");
+    }
+    public void doAction2() {
+        System.out.println("100hp");
+    }
+}
---
+++ src/StopState.java
@@ -0,0 +1,9 @@
+
+public class StopState implements State {
+    public void doAction() {
+        System.out.println("Stop State");
+    }
+    public void doAction2() {
+        System.out.println("20hp");
+    }
+}
